062b9cf39c063e5aba57234f6c8b55e7dc65af7c
Francisco Javier Martín Luque <jamartinluque8@gmail.com>
2025-03-19 19:57:10 +0100

    routes done, login empezado

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { LoginComponent } from './features/user_auth/login/login.component';
+import { RegisterComponent } from './features/user_auth/register/register.component';
+import { MatchesComponent } from './features/matches/matches.component';
+import { ProfileComponent } from './features/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    {path: 'home', component: HomeComponent},
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'matches', component: MatchesComponent},
+    {path: 'profile', component: ProfileComponent},
+    { path: '**', redirectTo: '/home' }
+
+];
