b5650fc65b2cb60d71588db5dda2f172418193d0
Francisco Javier Martín Luque <jamartinluque8@gmail.com>
2025-05-17 22:19:08 +0200

    docker-compose modified

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -10,14 +10,36 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatMenuPanel } from '@angular/material/menu';
+import { User } from './entities/user';
+import { CommonModule } from '@angular/common';
+import { ProfileService } from './features/profile/profile.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule, MatSidenavModule, MatListModule, MatExpansionModule],
+  imports: [RouterOutlet, RouterModule, MatToolbarModule, MatButtonModule, MatMenuModule, MatIconModule, MatSidenavModule, MatListModule, MatExpansionModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'frontend';
+
+  user: User | null = null;
+  
+  constructor(private profileService: ProfileService) { }
+
+    ngOnInit(): void {
+        const storedUserId = localStorage.getItem('userId');
+        const userId = storedUserId ? parseInt(storedUserId, 10) : null;
+        if (userId) {
+            this.profileService.getUserProfile(userId).subscribe({
+                next: (data: User) => {
+                    this.user = data;
+                },
+                error: () => {
+                    console.error('Error fetching user profile');
+                }
+            });
+        }
+    }
 }
